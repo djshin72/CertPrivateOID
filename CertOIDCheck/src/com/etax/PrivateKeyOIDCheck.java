31d10c4b0a417215c3b699e706727f7dfd762054
djshin72 <bluesoul72@gmail.com>
2017-01-06 18:52:20 +0900

    Update PrivateKeyOIDCheck.java
    인증서 이름 변경

--- CertOIDCheck/src/com/etax/PrivateKeyOIDCheck.java
+++ CertOIDCheck/src/com/etax/PrivateKeyOIDCheck.java
@@ -23,8 +23,8 @@ public class PrivateKeyOIDCheck {
 		//getPublicKey("C:/temp/cert/signCert_dj.der");
 		//getPrivateKey("C://temp//cert//signPri_dj.key");
 		
-		getPublicKey("signCert_yj.der");
-		getPrivateKey("signPri_yj.key");
+		getPublicKey("SignCert_yj.der");
+		getPrivateKey("SignPri_yj.key");
 		
 		//PublicKey publicKey = getPublicKey("signCert.der");
 		//getPrivateKey("signPri.key");
